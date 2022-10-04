a81346c1ce6e6f3f0de69a03b34ce89a73880afe
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-10-04 02:49:37 -0300

    Detalhes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
+
+//Details
+const divDetails = document.querySelector('.pokemon_details');
+const pokemon_type = document.querySelector('.pokemon_type');
+const pokemon_hp = document.querySelector('.pokemon_hp');
+const pokemon_atk = document.querySelector('.pokemon_atk');
+const pokemon_def = document.querySelector('.pokemon_def');
+const pokemon_speed = document.querySelector('.pokemon_speed');
+
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 const btnPrev = document.querySelector('.btn-prev');
@@ -26,6 +35,11 @@ const renderPokemon = async (pokemon) => {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
+        pokemon_type.innerHTML = data.types[0].type.name;
+        pokemon_hp.innerHTML = data.stats[0].base_stat;
+        pokemon_atk.innerHTML = data.stats[1].base_stat;
+        pokemon_def.innerHTML = data.stats[2].base_stat;
+        pokemon_speed.innerHTML = data.stats[5].base_stat;
         countPokemon = data.id;
         pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
         input.value = '';
@@ -53,4 +67,20 @@ btnNext.addEventListener('click', () => {
     renderPokemon(countPokemon);
 });
 
+pokemonImage.addEventListener('click', () => {
+    divDetails.style.display = 'block';
+});
+
+divDetails.addEventListener('click', () => {
+    divDetails.style.display = 'none';
+});
+
 renderPokemon(countPokemon);
+
+$('.pokemon_image').mouseover(function(){
+    $('.img_info').css('display', 'block');
+});
+  
+$('.pokemon_image').mouseout(function(){
+    $('.img_info').css('display', 'none');
+});
