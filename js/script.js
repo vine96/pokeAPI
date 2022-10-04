aad1abf52f5a4ccbb848f1d3f96661670e2a0a0e
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-10-04 04:12:50 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -25,6 +25,14 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
+const fetchPokemonTypes = async (type) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+    if(APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
 const renderPokemon = async (pokemon) => {
 
     pokemonName.innerHTML = 'carregando...';
@@ -49,6 +57,28 @@ const renderPokemon = async (pokemon) => {
         pokemonNumber.innerHTML = '';
     }
 }
+
+var pokeTypes = [];
+
+var obj = {
+    'type': [],
+    'qtd': []
+};
+
+console.log(obj.typeCount);
+
+const renderType = async (type) => {
+
+    const data = await fetchPokemonTypes(type);
+
+    if (data){
+        obj.type.push(data.name);
+        obj.qtd.push(data.pokemon.length);
+    }
+}
+
+var result = Object.entries(obj);
+console.log(result[0][1]);
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -83,4 +113,45 @@ $('.pokemon_image').mouseover(function(){
   
 $('.pokemon_image').mouseout(function(){
     $('.img_info').css('display', 'none');
+});
+
+for(i=1; i<=20; i++){
+    renderType(i);
+}
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
